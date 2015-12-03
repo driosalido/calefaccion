224ba037ce54db530e12e15df8e08754dc7448b1
driosalido <driosalido@gmail.com>
2015-12-03 16:44:51 +0100

    Nuevo Wemo-server

--- sbin/wemo-server.pl
+++ sbin/wemo-server.pl
@@ -5,15 +5,17 @@ use WebService::Belkin::WeMo::Device;
 use Mojolicious::Lite;
 use Mojo::JSON;
 use Data::Dumper;
-use Encode;
-use strict;
 
-helper reply => sub {
-    my $self    = shift;
-    my $message = shift;
-    my $reply = { response => $message };
-    return $self->render( json => $reply );
-};
+my $belkindb = "/etc/belkin.db";
+
+my @CMDS   = qw(test status commands on off);
+my %ROUTES = (
+    test     => 'test',
+    status   => 'status',
+    commands => 'list_commands',
+    on       => 'turn_on',
+    off      => 'turn_off',
+);
 
 get '/' => sub {
     my $self = shift;
@@ -22,37 +24,33 @@ get '/' => sub {
 
 post '/sms' => sub {
     my $self = shift;
-
-    my $data = {
-        "uas"  => $self->req->headers->user_agent,
-        "Body" => $self->param('Body'),
-    };
-
-    $self->app->log->info( Dumper($data) );
-
-    $self->res->headers->header( 'Content-Type' => 'application/json' );
-
-    my $belkindb     = "/etc/belkin.db";
-    my $wemoDiscover = WebService::Belkin::WeMo::Discover->new();
-    my $discovered   = $wemoDiscover->load($belkindb);
-
-    my $command = $self->param('Body');
-
-    $self->app->log->info("Got command - $command");
-
-    my $valid_commands = {
-        '1' => 'test',
-        '2' => 'status',
-        '3' => 'commands',
-	'4' => '<device|all> <on|off>',
-    };
-
-    if ( lc($command) eq "test" ) {
-        $self->reply("Works!");
+    my $msg  = $self->param('Body');
+    $self->reply("ERROR - Bad Command") unless $msg;
+    $self->app->log->info("Got Message - $msg");
+    $msg =~ m/^([^ ]*)/;
+    my $command = lc($1);
+    if ( grep $_ eq $command, @CMDS ) {
+        $self->app->log->info("Got Command [$command]");
+        my $helper = $ROUTES{$command};
+        $self->$helper($msg);
     }
-    elsif ( lc($command) eq "status" ) {
+    else {
+        $self->reply("ERROR - Bad Command");
+    }
+};
 
-        my $devices;
+###COMMANDS
+
+helper test => sub {
+    my $self = shift;
+    return $self->reply("I'm working!");
+};
+
+helper status => sub {
+    my $self = shift;
+    my $devices;
+    my $discovered = $self->discover;
+    if ($discovered) {
         foreach my $ip ( keys %{$discovered} ) {
             my $wemo = WebService::Belkin::WeMo::Device->new(
                 ip => $ip,
@@ -62,82 +60,138 @@ post '/sms' => sub {
             $devices .= "[" . $wemo->getBinaryState() . "],";
         }
         chop $devices;
-
         $self->reply($devices);
-
     }
-    elsif ( lc($command) eq "commands" ) {
-
-        my $commands;
-        foreach my $cmd ( values %{$valid_commands} ) {
-            $commands .= "$cmd,";
-        }
-        chop $commands;
-
-        $self->reply($commands);
-
+    else {
+        return $self->reply("Problem discovering WEMO Devices");
     }
-    elsif ( lc($command) =~ m/(\w+) (on|off)/g ) {
+};
 
-        my $target = $1;
-        my $action = $2;
-        $self->app->log->info("t = $target, o = $action");
+helper list_commands => sub {
+    my $self = shift;
+    my $commands;
+    foreach (@CMDS) {
+        $commands .= "$_,";
+    }
+    chop $commands;
+    $self->reply($commands);
+};
 
+helper turn_on => sub {
+    my ( $self, $msg ) = @_;
+    if ( $msg =~ m/^([^ ]*) *([^ ]*)/g ) {
+        my $device = $2;
+        $self->app->log->info("Got Device $device");
+        $self->turn( $device, 'on' );
+        $self->status;
+    }
+    else {
+        $self->reply("Device Missing , use 'on device' or 'on all'");
+    }
+};
+
+helper turn_off => sub {
+    my ( $self, $msg ) = @_;
+    if ( $msg =~ m/^([^ ]*) *([^ ]*)/g ) {
+        my $device = $2;
+        $self->app->log->info("Got Device [$device]");
+        $self->turn( $device, 'off' );
+    }
+    else {
+        $self->reply("Device Missing , use 'off device' or 'off all'");
+    }
+};
+
+###REAL HELPERS
+helper discover => sub {
+    my $self         = shift;
+    my $wemoDiscover = WebService::Belkin::WeMo::Discover->new();
+    my $wemo;
+    eval { $wemo = $wemoDiscover->load($belkindb); };
+    $self->app->log->error("Failed to discover WEMO Devices $@");
+    return $wemo;
+};
+
+helper turn => sub {
+    my ( $self, $target, $action ) = @_;
+    my $discovered = $self->discover;
+    if ($discovered) {
+
+        #Let's turn on|off all of them...
         if ( $target eq "all" ) {
-
+            $self->app->log->info("Turning $action every WEMO Device");
             foreach my $ip ( keys %{$discovered} ) {
-
                 if ( $discovered->{$ip}->{'type'} eq "switch" ) {
-
                     my $wemo = WebService::Belkin::WeMo::Device->new(
                         ip => $ip,
                         db => $belkindb
                     );
-
                     if ( $action eq "off" ) {
                         $self->app->log->info("Turning off wemo $target");
                         $wemo->off();
                     }
                     if ( $action eq "on" ) {
-			$self->app->log->info("Turning on wemo $target");
+                        $self->app->log->info("Turning on wemo $target");
                         $wemo->on();
                     }
                 }
             }
-            $self->reply("OK");
         }
         else {
             my $wemo;
             eval {
-                $wemo = WebService::Belkin::WeMo::Device->new(name => $target,db   => $belkindb);
+                $wemo = WebService::Belkin::WeMo::Device->new(
+                    name => $target,
+                    db   => $belkindb
+                );
                 1;
             };
-            if ($@){
+            if ($@) {
                 $self->reply("ERROR - Device unknown");
-                $self->app->log->info("Error al invocar WEMO [$!]");
-            } else {
-                $self->app->log->info("$action for wemo $_");
+                $self->app->log->error("Error al invocar WEMO [$@]");
+            }
+            else {
+                $self->app->log->info("Turning $action for wemo $_");
                 if ( $action eq "off" ) {
                     $wemo->off();
                 }
                 if ( $action eq "on" ) {
                     $wemo->on();
                 }
-                $self->reply("OK");
             }
         }
-    } else {
-        $self->reply("ERROR - Bad Command");
     }
-
+    else {
+        #Nothing discovered
+        return $self->reply("Problem discovering WEMO Devices");
+    }
 };
 
-app->config( hypnotoad => { 
-	listen => ['http://*:9000'],
-	pid_file => '/run/wemo-server.pid',
-	workers => 1,
-} );
+helper reply => sub {
+    my $self    = shift;
+    my $message = shift;
+    my $reply   = { response => $message };
+    $self->res->headers->header( 'Content-Type' => 'application/json' );
+    return $self->render( json => $reply );
+};
 
-app->log->path('/var/log/wemo-server.log');
+helper change_status => sub {
+    my $self   = shift;
+    my $device = shift;
+    my $status = shift;
+};
+
+###CONFIGS
+
+app->config(
+    hypnotoad => {
+        listen => ['http://*:9000'],
+
+        #	pid_file => '/run/wemo-server.pid',
+        workers => 1,
+    }
+);
+
+app->log->path('log/wemo-server.log');
 app->log->level('info');
 app->start;
